4123131665b585e42c8e6afcd3365bb352676eb3
jvaquero <jvaquero@aidacanarias.com>
2022-02-21 07:37:53 +0000

    Agregar archivos de proyecto.

---
+++ StringCalculator.Tests/StringCalculatorShould.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringCalculator.Tests {
+    public class StringCalculatorShould {
+        [Test]
+        public void not_be_instanciable()
+        {
+            var calculator = new StringCalculator();
+
+            Assert.Fail("Should not reach this point due a previous compilation error when calling the ctor.");
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace StringCalculator {
+    public class StringCalculator {
+    }
+}
