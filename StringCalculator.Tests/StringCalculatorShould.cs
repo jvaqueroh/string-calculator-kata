e0b5e2ae7ec32eda77f349e13a52ff677181dd55
jvaquero <jvaquero@aidacanarias.com>
2022-02-28 08:19:16 +0000

    refactor: add SetUp method in tests

--- StringCalculator.Tests/StringCalculatorShould.cs
+++ StringCalculator.Tests/StringCalculatorShould.cs
@@ -4,10 +4,17 @@ using NUnit.Framework;
 
 namespace StringCalculator.Tests {
     public class StringCalculatorShould {
+        private StringCalculator calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            calculator = new StringCalculator();
+        }
+
         [Test]
         public void return_0_for_an_empty_string()
         {
-            var calculator = new StringCalculator();
             var result = calculator.Add("");
             result.Should().Be(0);
         }
@@ -15,7 +22,6 @@ namespace StringCalculator.Tests {
         [Test]
         public void return_same_number_for_an_input_with_only_one()
         {
-            var calculator = new StringCalculator();
             var result = calculator.Add("7");
             result.Should().Be(7);
         }
@@ -23,7 +29,6 @@ namespace StringCalculator.Tests {
         [Test]
         public void return_the_sum_for_an_input_with_two_comma_separated_values()
         {
-            var calculator = new StringCalculator();
             var result = calculator.Add("7,6");
             result.Should().Be(13);
         }
