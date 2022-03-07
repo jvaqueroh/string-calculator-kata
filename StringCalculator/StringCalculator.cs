5dabc571d608e1ddca18a66bebe7a0e099bca65e
jvaquero <jvaquero@aidacanarias.com>
2022-03-07 07:29:22 +0000

    refactor: extract field for default separators

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -3,20 +3,23 @@ using System.Linq;
 
 namespace StringCalculator {
     public class StringCalculator {
+        private readonly string[] defatultSeparators = new string[] { ",", "\n" };
+
         public int Add(string numbers)
         {
             if(string.IsNullOrWhiteSpace(numbers))
                 return 0;
-            var defaultSeparators = new string[] { ",", "\n" };
+            
+            var customSeparators = defatultSeparators;
             if (numbers.StartsWith("//"))
             {
                 var lines = numbers.Split("\n");
                 var customSeparator = lines[0].Substring(2, 1);
-                defaultSeparators = defaultSeparators.Append(customSeparator).ToArray();
+                customSeparators = defatultSeparators.Append(customSeparator).ToArray();
                 numbers = numbers.Substring(lines[0].Length-1);
             }
 
-            var values = numbers.Split(defaultSeparators, StringSplitOptions.RemoveEmptyEntries);
+            var values = numbers.Split(customSeparators, StringSplitOptions.RemoveEmptyEntries);
             return values.Sum(int.Parse);
         }
     }
