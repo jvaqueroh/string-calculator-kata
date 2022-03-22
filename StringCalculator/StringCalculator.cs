f3a52729ba23cd6b8a79b1008f78854ea08132bd
jvaquero <jvaquero@aidacanarias.com>
2022-03-22 07:12:27 +0000

    refactor - extract method

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -10,14 +10,21 @@ namespace StringCalculator {
                 return 0;
             var separators = new List<char>() { ',', '\n' };
 
+            numbers = ProcessHeader(numbers, separators);
+
+            var numbersList = numbers.Split(separators.ToArray());
+            return numbersList.Sum(int.Parse);
+        }
+
+        private static string ProcessHeader(string numbers, List<char> separators)
+        {
             if (numbers.StartsWith("//"))
             {
                 separators.Add(char.Parse(numbers.Substring(2, 1)));
                 numbers = numbers.Substring(4);
             }
 
-            var numbersList = numbers.Split(separators.ToArray());
-            return numbersList.Sum(int.Parse);
+            return numbers;
         }
     }
 }
