8cf45b25b2bfe659e2b1d012bc8401cf38c1d492
jvaquero <jvaquero@aidacanarias.com>
2022-03-22 07:09:55 +0000

    feat return_the_sum_for_an_input_with_custom_separator_in_a_header_line

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -8,6 +8,13 @@ namespace StringCalculator {
             if(string.IsNullOrWhiteSpace(numbers))
                 return 0;
             var separators = new[] { ',', '\n' };
+
+            if (numbers.StartsWith("//"))
+            {
+                separators = separators.Append(char.Parse(numbers.Substring(2, 1))).ToArray();
+                numbers = numbers.Substring(4);
+            }
+
             var numbersList = numbers.Split(separators);
             return numbersList.Sum(int.Parse);
         }
