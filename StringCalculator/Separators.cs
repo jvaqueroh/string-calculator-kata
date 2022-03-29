1223d0f88785ee29fc6db0cb5f130bb95065f244
jvaquero <jvaquero@aidacanarias.com>
2022-03-29 07:14:56 +0100

    refactor: extract Separators class

---
+++ StringCalculator/Separators.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace StringCalculator
+{
+    public class Separators
+    {
+        private List<string> Value { get; } = new List<string>() { ",", "\n" };
+
+        public void Add(string newSeparator) {
+            if(!string.IsNullOrWhiteSpace(newSeparator))
+                Value.Add(newSeparator);
+        }
+
+        public void Add(List<string> newSeparators) {
+            newSeparators.ForEach(Add);
+        }
+
+        public string[] ToArray() {
+            return Value.ToArray();
+        }
+    }
+}
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,27 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace StringCalculator {
-    public class Separators
-    {
-        private List<string> Value { get; } = new List<string>() { ",", "\n" };
-
-        public void Add(string newSeparator) {
-            if(!string.IsNullOrWhiteSpace(newSeparator))
-                Value.Add(newSeparator);
-        }
-
-        public void Add(List<string> newSeparators) {
-            newSeparators.ForEach(Add);
-        }
-
-        public string[] ToArray() {
-            return Value.ToArray();
-        }
-    }
-
     public class StringCalculator {
         private readonly Separators separators = new Separators();
 
